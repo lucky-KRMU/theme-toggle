bc92d56eb5d5da85d87234d2b556f4b509d80dbb
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-11 10:30:00 +0000

    add Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     Hello World!
+    <Card />
     </>
   )
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+    This is a card
+    </>
+  )
+}
+
+export default Card
