0df963a68cf571af23d60284315daca0361edd9a
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-11 15:58:38 +0000

    fix html class change bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
+import ThemeContextProvider from './Context/ThemeContextProvider'
 
 function App() {
 
   return (
     <>
-    <div className='h-screen w-full flex items-center justify-center flex-col gap-5 bg-sky-700 font-[Pliant]'>
+      <ThemeContextProvider>
+        <div className='h-screen w-full flex items-center justify-center flex-col gap-5 bg-sky-700 font-[Pliant]'>
 
-    <h1 className='text-5xl font-semibold text-white'>Theme Toggle</h1>
-    <Card />
-    </div>
+          <h1 className='text-5xl font-semibold text-white'>Theme Toggle</h1>
+          <Card />
+        </div>
+      </ThemeContextProvider>
     </>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import ThemeContextProvider from '../Context/ThemeContextProvider'
+import ThemeContext from '../Context/theme';
 
 function Card() {
+
+    // let themeContext = useContext(ThemeContextProvider);
+    let themeContext = useContext(ThemeContext);
+
+    let [theme, setTheme] = useState(themeContext.theme);
+    // let [themeState, setThemeState] = useState(themeContext.theme)
+
+    const handleToggle = () => {
+        if (theme === "light") {
+            themeContext.theme = "dark"
+            setTheme(themeContext.theme);
+        }
+        else {
+            themeContext.theme = "light"
+            setTheme(themeContext.theme);
+        }
+        console.log(theme);
+        console.log(themeContext);
+    }
+
+    useEffect(()=>{
+        let html = document.querySelector('html')
+        if(theme === "light"){
+            html.className = 'dark';
+        } else{
+            html.className = 'light';
+        }
+    }, [theme])
+
     return (
         <>
             <div className='h-[50vh] w-[20vw] border-double border-white border-4 rounded-2xl text-center flex items-center justify-center flex-col gap-4 p-3'>
@@ -9,9 +40,9 @@ function Card() {
 
 
                 <label className="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="sr-only peer" />
-                        <div className="relative w-9 h-5 bg-neutral-quaternary outline-4 outline-black peer-focus:outline-2 peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-500"></div>
-                        {/* <span class="select-none ms-3 text-sm font-medium text-heading">Toggle me</span> */}
+                    <input type="checkbox" value={theme} className="sr-only peer" onChange={() => handleToggle()} onToggle={handleToggle} />
+                    <div className="relative w-9 h-5 bg-neutral-quaternary outline-4 outline-black peer-focus:outline-2 peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-500"></div>
+                    {/* <span class="select-none ms-3 text-sm font-medium text-heading">Toggle me</span> */}
                 </label>
 
 
