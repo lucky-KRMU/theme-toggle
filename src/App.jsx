30c811eed849a74fb69bc23b04bc687602981fce
lucky-KRMU <2501730142@krmu.edu.in>
2026-06-11 16:07:15 +0000

    add final UI update - Project Complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <ThemeContextProvider>
-        <div className='h-screen w-full flex items-center justify-center flex-col gap-5 bg-sky-700 font-[Pliant]'>
+        <div className='h-screen w-full flex items-center justify-center flex-col gap-5 bg-sky-700 font-[Pliant] dark:bg-black duration-300'>
 
           <h1 className='text-5xl font-semibold text-white'>Theme Toggle</h1>
           <Card />
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -34,14 +34,14 @@ function Card() {
 
     return (
         <>
-            <div className='h-[50vh] w-[20vw] border-double border-white border-4 rounded-2xl text-center flex items-center justify-center flex-col gap-4 p-3'>
-                <h3 className='text-white text-2xl'>Toggle to change Theme</h3>
+            <div className='h-[50vh] w-[50vw] md:w-[20vw] border-double border-white border-4 rounded-2xl text-center flex items-center justify-center flex-col gap-4 p-3 dark:border-dashed duration-300'>
+                <h3 className='text-white font-semibold text-2xl dark:text-orange-500 duration-300'>Toggle to change Theme</h3>
 
 
 
                 <label className="inline-flex items-center cursor-pointer">
                     <input type="checkbox" value={theme} className="sr-only peer" onChange={() => handleToggle()} onToggle={handleToggle} />
-                    <div className="relative w-9 h-5 bg-neutral-quaternary outline-4 outline-black peer-focus:outline-2 peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-500"></div>
+                    <div className="relative w-9 h-5 bg-neutral-quaternary outline-4 outline-black peer-focus:outline-2 peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-0.5 after:inset-s-0.5 after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-500 peer-checked:outline-white"></div>
                     {/* <span class="select-none ms-3 text-sm font-medium text-heading">Toggle me</span> */}
                 </label>
 
